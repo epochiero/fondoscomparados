8740e4858455283697780b5e496a0c3ff9af3e30
Ezequiel Pochiero <epochiero@gmail.com>
2014-06-09 15:34:54 -0300

    Implementación del nuevo diseño

--- fondoscomparados/apps/viviendas/views.py
+++ fondoscomparados/apps/viviendas/views.py
@@ -4,8 +4,13 @@ from models import *
 
 
 def index(request, template="index.html"):
-    data= {}
-    data['ba'] = ProvinciaPorAnio.objects.filter(provincia__nombre="Buenos Aires")
-    data['sf'] = ProvinciaPorAnio.objects.filter(provincia__nombre="Santa Fe")
-    data['er'] = ProvinciaPorAnio.objects.filter(provincia__nombre="Entre Rios")
+    try:
+        year = int(request.GET.get('year', 2006))
+    except ValueError:
+        year = 2006
+    data = {}
+    data['cba'] = ProvinciaPorAnio.objects.filter(provincia__nombre="Cordoba", anio=year)
+    data['sf'] = ProvinciaPorAnio.objects.filter(provincia__nombre="Santa Fe", anio=year)
+    data['er'] = ProvinciaPorAnio.objects.filter(provincia__nombre="Entre Rios", anio=year)
+    data['year'] = year
     return render_to_response(template, data, context_instance=RequestContext(request))
